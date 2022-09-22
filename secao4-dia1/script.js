918072be0a8c1092763fae7e981a6bcf93cca6f8
Mateus Kulik <mateuskulik30@gmail.com>
2022-09-22 15:01:04 -0300

    Adiciona novo exercicio

--- secao4-dia1/script.js
+++ secao4-dia1/script.js
@@ -10,12 +10,20 @@
 //     console.log('iza é a mais jovem, ela tem '  +  iza +  ' anos.') 
 // }
 
-let a = 16;
-let b = 89;
-let c = 90;
+ // let a = 16;
+// let b = 89;
+// let c = 90;
 
-if (Number[a + b + c == 180]) {
-    console.log('Você tem um triângulo perfeito.')
-} else {
-    console.log('Você não tem um triângulo.')
-}
+// if (Number[a + b + c == 180]) {
+//     console.log('Você tem um triângulo perfeito.')
+// } else {
+//     console.log('Você não tem um triângulo.')
+// }
+
+let numero1 = 20;
+let numero2 = 17;
+let numero3 = 13;
+
+if (numero1 % numero2 === 0 && numero1 % numero3 == 0 ) {
+    console.log(true);
+} else if ( )
