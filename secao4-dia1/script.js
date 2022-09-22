3e3e9308feee613925e5a52bdf6e9cfab2ad9c72
Mateus Kulik <mateuskulik30@gmail.com>
2022-09-22 14:17:20 -0300

    Adiciona exercicio de condicoes if/else

--- secao4-dia1/script.js
+++ secao4-dia1/script.js
@@ -1,5 +1,11 @@
-let name = 'Mat';
+// let marina = 23;
+// let silvia = 26;
+// let iza = 20;
 
-
-console.log(name);
-
+// if (marina < silvia && marina < iza)  {
+//     console.log('Marina é a mais jovem, ela possui' + marina + 'anos')
+// } else if (silvia < marina && silvia < iza) {
+//     console.log('Silvia é a mais jovem, ela tem ' + silvia + ' anos.')
+// } else {
+//     console.log('iza é a mais jovem, ela tem '  +  iza +  ' anos.') 
+// }
