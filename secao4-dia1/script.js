963f16fdfb9fa4f3e7893239849778e74794dfe1
Mateus Kulik <mateuskulik30@gmail.com>
2022-09-25 15:31:35 -0300

    Adiciona exercicio de comparacao

--- secao4-dia1/script.js
+++ secao4-dia1/script.js
@@ -138,10 +138,22 @@
 
 //===================================================
 
-let numbers =[];
-let divisiveis = 0;
-for(let index = 0; index <= 150; index += 1) {
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
 }
 
-console.log(divisiveis);
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
 
+console.log(biggestWord);
+console.log(smallestWord);
