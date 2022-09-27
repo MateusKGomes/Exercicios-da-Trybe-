ab506a6e9d3580bbb574b1e7853ec5ebe73d9a8f
Mateus Kulik <mateuskulik30@gmail.com>
2022-09-26 21:40:27 -0300

    Adiciona exercicios de objetos

--- funcoes-objetos/scripit.js
+++ funcoes-objetos/scripit.js
@@ -27,3 +27,72 @@
 // console.log(sum(10, 5));
 // console.log(sum(1555, 848125));
 
+// function verificaPalindrome (word1, word2){
+//     if(word1 === word1) {
+//         return 'Você é uma palindrome'
+//     }else if(word2 === word2.reverse){
+//         return 'Você é uma palindrome'
+//     }else {
+//         return 'Você nao é uma palindrome.'
+//     };
+// }   
+// console.log(verificaPalindrome('arara', 'desenvolvimento'));
+ //==============================================================
+
+// function numbers(reviewNumbers){
+//     let biggestIndex = 0;
+// }
+//     for(let index =0; index <= reviewNumbers.length; index += 1){
+//         if(reviewNumbers[index].length > reviewNumbers.length) {
+//             biggestIndex += 1;
+
+//         }
+//     }
+
+// reviewNumbers = [2, 3, 6, 7, 10, 1];
+
+
+// console.log(biggestIndex);
+
+//==============================================================
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info.recorrente = 'Sim';
+// for(let key in info){   
+//     console.log(info[key])
+// }
+
+
+//==============================================================
+
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+
+// leitor.livrosFavoritos.push({
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor:  'JK Rowling',
+//     editora:  'Rocco',
+// });
+// console.log(leitor);
+
+//   console.log('O livro favorito de ' + leitor.nome + ' ' +leitor.sobrenome + ' se chama '+ leitor.livrosFavoritos[0].autor); 
+
+// console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+
+
+
