1ef8200d96a8c5d6e2b03752e2f49ea3ef03049c
Mateus Kulik <mateuskulik30@gmail.com>
2022-10-20 11:02:48 -0300

    exercício 6

--- jest/script.js
+++ jest/script.js
@@ -17,19 +17,44 @@
   
 // module.exports = myRemove; 
 
-const techList = (arr, name) => {
-    if(arr.length === 0)
-        return 'Vazio!';
+// const techList = (arr, name) => {
+//     if(arr.length === 0)
+//         return 'Vazio!';
     
-    const arrayTech = arr.sort();
-    const nameTech = [];
-    for(let index = 0; index < arr.length; index += 1){
-        nameTech.push({
-            tech: arrayTech[index],
-            name,
-        });
-    }
-    return nameTech;
-}
+//     const arrayTech = arr.sort();
+//     const nameTech = [];
+//     for(let index = 0; index < arr.length; index += 1){
+//         nameTech.push({
+//             tech: arrayTech[index],
+//             name,
+//         });
+//     }
+//     return nameTech;
+// }
 
-module.exports = techList;
+// module.exports = techList;
+
+// hydrate.js
+
+const hydrate = (string) => {
+    const splitString = string.split('');
+    let glassesOfWater = 0;
+  
+    for (let index = 0; index < splitString.length; index += 1) {
+      const parsedCharacter = parseInt(splitString[index]);
+  
+      if (parsedCharacter) {
+        glassesOfWater += parsedCharacter;
+      }
+    }
+  
+    let glass = 'copo';
+  
+    if (glassesOfWater > 1) {
+      glass = 'copos';
+    }
+  
+    return `${glassesOfWater} ${glass} de água`;
+  };
+  
+  module.exports = hydrate;
