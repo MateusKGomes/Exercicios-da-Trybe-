65d30492ae37c85e42851e627d4adce72ed36760
Mateus Kulik <mateuskulik30@gmail.com>
2022-10-25 08:56:55 -0300

    sorteador de número

--- hof/scripit.js
+++ hof/scripit.js
@@ -1,15 +1,21 @@
-const newEmployees = (employeeGenerator) => {
-  const employees = {
-    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: employeeGenerator('Carla Paiva'),  // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  };
-  return employees;
+// const newEmployees = (employeeGenerator) => {
+//   const employees = {
+//     id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: employeeGenerator('Carla Paiva'),  // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   };
+//   return employees;
+// };
+
+//   const employeeGenerator = (fullName) => {
+//     const email = fullName.toLowerCase().split(' ').join('_');
+//     return { fullName, email: `${email}@trybe.com`}
+//   };
+
+// console.log(newEmployees(employeeGenerator)); 
+const checkNumbers = (myNumber, number) => myNumber === number; 
+const randomNumber = (myNumber, callBack ) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+  return callBack(myNumber, number) ? 'You won!' : `Try again, drawn number is ${number}`;
 };
-
-  const employeeGenerator = (fullName) => {
-    const email = fullName.toLowerCase().split(' ').join('_');
-    return { fullName, email: `${email}@trybe.com`}
-  };
-
-console.log(newEmployees(employeeGenerator));
+console.log(randomNumber(3, checkNumbers));
